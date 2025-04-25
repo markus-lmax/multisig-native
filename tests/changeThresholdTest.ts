a37255b29fd6a4b796c447adf456edd962e4b4af
stoym <stoym@lmax.com>
2025-04-25 18:16:14 +0100

    backfill almost all remaining changeThreshold tests from anchor version

--- tests/changeThresholdTest.ts
+++ tests/changeThresholdTest.ts
@@ -14,14 +14,114 @@ describe("change threshold", async () => {
     const [ownerA, ownerB, _ownerC] = multisig.owners;
 
     const changeThreshold = dsl.createChangeThresholdInstruction(multisig.address, 3);
-    const [transactionAddress, _txMeta] = await dsl.proposeTransaction(ownerA, [changeThreshold], multisig.address);
-    await dsl.approveTransaction(ownerB, multisig.address, transactionAddress);
-    await dsl.executeTransaction(transactionAddress, changeThreshold, multisig.signer, multisig.address, ownerB, ownerA.publicKey);
+    const [txAddress, _txMeta] = await dsl.proposeTransaction(ownerA, [changeThreshold], multisig.address);
+    await dsl.approveTransaction(ownerB, multisig.address, txAddress);
+    await dsl.executeTransaction(txAddress, changeThreshold, multisig.signer, multisig.address, ownerB, ownerA.publicKey);
 
     const actualMultisig = await dsl.getMultisig(multisig.address);
     assert.strictEqual(actualMultisig["nonce"], multisig.nonce);
     assert.strictEqual(actualMultisig["threshold"], 3);
     assert.deepEqual(actualMultisig["owners"], multisig.owners.map(owner => Array.from(owner.publicKey.toBytes())));
     assert.strictEqual(actualMultisig["owner_set_seqno"], 0);
+  });
+
+  test("should require new threshold to be met", async () => {
+    const multisig = await dsl.createMultisig(2, 3);
+    const [ownerA, ownerB, ownerC] = multisig.owners;
+
+    const changeThresholdTo3 = dsl.createChangeThresholdInstruction(multisig.address, 3);
+    const [txAddress, _txMeta] = await dsl.proposeTransaction(ownerA, [changeThresholdTo3], multisig.address);
+    await dsl.approveTransaction(ownerB, multisig.address, txAddress);
+    await dsl.executeTransaction(txAddress, changeThresholdTo3, multisig.signer, multisig.address, ownerB, ownerA.publicKey);
+
+    const changeThresholdTo2 = dsl.createChangeThresholdInstruction(multisig.address, 2);
+    const [txAddress2, _txMeta2] = await dsl.proposeTransaction(ownerA, [changeThresholdTo2], multisig.address);
+    await dsl.approveTransaction(ownerB, multisig.address, txAddress2);
+
+    // Fail when trying to execute with the old threshold
+    let txResult = await dsl.executeTransaction(txAddress2, changeThresholdTo2, multisig.signer, multisig.address, ownerB, ownerA.publicKey);
+    assert.ok(txResult.meta.logMessages.includes("Program log: assertion failed - program error: NotEnoughSigners (The transaction must reach a minimum number of approvals.)"))
+    assert.strictEqual(txResult.result, "Error processing Instruction 0: custom program error: 0xd");
+
+    //Succeed when reaching the new threshold
+    await dsl.approveTransaction(ownerC, multisig.address, txAddress2);
+    await dsl.executeTransaction(txAddress2, changeThresholdTo2, multisig.signer, multisig.address, ownerB, ownerA.publicKey);
+    const actualMultisig = await dsl.getMultisig(multisig.address);
+    assert.strictEqual(actualMultisig["threshold"], 2);
+  });
+
+  test("should update threshold for new transactions once executed", async () => {
+    const multisig = await dsl.createMultisig(2, 3);
+    const [ownerA, ownerB, _ownerC] = multisig.owners;
+
+    const changeThreshold = dsl.createChangeThresholdInstruction(multisig.address, 1);
+    const [txAddress, _txMeta] = await dsl.proposeTransaction(ownerA, [changeThreshold], multisig.address);
+
+    // actual threshold not updated whilst tx in flight
+    let inFlightThreshold = (await dsl.getMultisig(multisig.address))["threshold"];
+    assert.strictEqual(inFlightThreshold, 2);
+
+    // so we have 2 approvals
+    await dsl.approveTransaction(ownerB, multisig.address, txAddress);
+    await dsl.executeTransaction(txAddress, changeThreshold, multisig.signer, multisig.address, ownerB, ownerA.publicKey);
+
+    //The already existing transaction has now been executed and should update the threshold to 1
+    let updatedThreshold = (await dsl.getMultisig(multisig.address))["threshold"];
+    assert.strictEqual(updatedThreshold, 1);
+  });
+
+  test("should use new threshold on an already existing transaction", async () => {
+    const multisig = await dsl.createMultisig(2, 3);
+    const [ownerA, ownerB, _ownerC] = multisig.owners;
+
+    const changeThresholdTo1 = dsl.createChangeThresholdInstruction(multisig.address, 1);
+    const [txAddress, _txMeta] = await dsl.proposeTransaction(ownerA, [changeThresholdTo1], multisig.address);
+
+    const changeThresholdTo3 = dsl.createChangeThresholdInstruction(multisig.address, 3);
+    const [txAddress2, _txMeta2] = await dsl.proposeTransaction(ownerA, [changeThresholdTo3], multisig.address);
+
+    await dsl.approveTransaction(ownerB, multisig.address, txAddress);
+    await dsl.executeTransaction(txAddress, changeThresholdTo1, multisig.signer, multisig.address, ownerB, ownerA.publicKey);
+
+    // threshold now updated to 1
+    let updatedThreshold = (await dsl.getMultisig(multisig.address))["threshold"];
+    assert.strictEqual(updatedThreshold, 1);
+
+    // threshold should now be set to 1 meaning that transaction 2 has met the 1/3 approval required for execution, and does not need a second approval
+    await dsl.executeTransaction(txAddress2, changeThresholdTo3, multisig.signer, multisig.address, ownerB, ownerA.publicKey);
+
+    // the second threshold update transaction has now been executed and should update the threshold to 3
+    let updatedThreshold2 = (await dsl.getMultisig(multisig.address))["threshold"];
+    assert.strictEqual(updatedThreshold2, 3);
+  });
+
+  test("should not allow 0 threshold", async () => {
+    const multisig = await dsl.createMultisig(2, 3);
+    const [ownerA, ownerB, _ownerC] = multisig.owners;
+
+    const changeThreshold = dsl.createChangeThresholdInstruction(multisig.address, 0);
+    const [txAddress, _txMeta] = await dsl.proposeTransaction(ownerA, [changeThreshold], multisig.address);
+    await dsl.approveTransaction(ownerB, multisig.address, txAddress);
+    let txResult = await dsl.executeTransaction(txAddress, changeThreshold, multisig.signer, multisig.address, ownerB, ownerA.publicKey);
+    assert.ok(txResult.meta.logMessages.includes("Program log: assertion failed - program error: InvalidThreshold (Threshold must be less than or equal to the number of owners and greater than zero.)"))
+    assert.strictEqual(txResult.result, "Error processing Instruction 0: custom program error: 0x0");
+
+    let actualThreshold = (await dsl.getMultisig(multisig.address))["threshold"];
+    assert.strictEqual(actualThreshold, 2);
+  });
+
+  test("should not allow threshold greater than number of owners", async () => {
+    const multisig = await dsl.createMultisig(2, 3);
+    const [ownerA, ownerB, _ownerC] = multisig.owners;
+
+    const changeThreshold = dsl.createChangeThresholdInstruction(multisig.address, 4);
+    const [txAddress, _txMeta] = await dsl.proposeTransaction(ownerA, [changeThreshold], multisig.address);
+    await dsl.approveTransaction(ownerB, multisig.address, txAddress);
+    let txResult = await dsl.executeTransaction(txAddress, changeThreshold, multisig.signer, multisig.address, ownerB, ownerA.publicKey);
+    assert.ok(txResult.meta.logMessages.includes("Program log: assertion failed - program error: InvalidThreshold (Threshold must be less than or equal to the number of owners and greater than zero.)"))
+    assert.strictEqual(txResult.result, "Error processing Instruction 0: custom program error: 0x0");
+
+    let actualThreshold = (await dsl.getMultisig(multisig.address))["threshold"];
+    assert.strictEqual(actualThreshold, 2);
   });
 });
