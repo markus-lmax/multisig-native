a11acc2aa846aab8558f75d85e104ba55d81a118
stoym <stoym@lmax.com>
2026-01-30 15:55:07 +0000

    extract validation into separate method

--- program/src/instructions/execute_transaction.rs
+++ program/src/instructions/execute_transaction.rs
@@ -5,15 +5,53 @@ use crate::state::transaction::Transaction;
 use borsh::BorshDeserialize;
 use solana_program::account_info::next_account_info;
 use solana_program::instruction::Instruction;
+use solana_program::program_error::ProgramError;
 use solana_program::pubkey::Pubkey;
 use solana_program::{account_info::AccountInfo, entrypoint::ProgramResult, msg};
 
-pub fn execute_transaction(program_id: &Pubkey, accounts: &[AccountInfo]) -> ProgramResult {
+pub fn execute_transaction(_program_id: &Pubkey, accounts: &[AccountInfo]) -> ProgramResult {
     msg!("invoke execute_transaction");
 
-    // TODO extract 3 methods from the below sections
+    let validated = validate(accounts)?;
 
-    // validate accounts
+    let signer_seeds = &[(*validated.multisig_account.key).as_ref(), &[validated.multisig.nonce]];
+    validated
+        .transaction
+        .instructions
+        .iter()
+        .map(|ix| {
+            let mut ix: Instruction = ix.into();
+            ix.accounts = ix
+                .accounts
+                .iter()
+                .map(|acc| {
+                    let mut acc = acc.clone();
+                    if acc.pubkey == *validated.multisig_signer.key {
+                        acc.is_signer = true;
+                    }
+                    acc
+                })
+                .collect();
+            solana_program::program::invoke_signed(&ix, accounts, &[&signer_seeds[..]])
+        })
+        .collect::<Result<Vec<_>, _>>()?;
+
+    close_account(validated.transaction_account, validated.refundee)
+}
+
+struct ValidatedAccounts<'a, 'b> {
+    multisig_account: &'a AccountInfo<'b>,
+    multisig_signer: &'a AccountInfo<'b>,
+    transaction_account: &'a AccountInfo<'b>,
+    refundee: &'a AccountInfo<'b>,
+    multisig: Multisig,
+    transaction: Transaction,
+}
+
+fn validate<'a, 'b>(accounts: &'a [AccountInfo<'b>]) -> Result<ValidatedAccounts<'a, 'b>, ProgramError>
+where
+    'b: 'a,
+{
     let accounts_iter = &mut accounts.iter();
     let multisig_account = next_account_info(accounts_iter)?;
     let multisig_signer = next_account_info(accounts_iter)?;
@@ -24,48 +62,20 @@ pub fn execute_transaction(program_id: &Pubkey, accounts: &[AccountInfo]) -> Pro
     let multisig = Multisig::try_from_slice(&multisig_account.data.borrow())?;
     let transaction = assert_success(
         Transaction::try_from_slice(&transaction_account.data.borrow()),
-        MultisigError::MalformedTransactionAccount
+        MultisigError::MalformedTransactionAccount,
     )?;
-
     assert_that(executor.is_signer && multisig.owners.contains(executor.key), MultisigError::InvalidExecutor)?;
     assert_that(multisig.owner_set_seqno == transaction.owner_set_seqno, MultisigError::InvalidOwnerSetSequenceNumber)?;
 
-    validate_signer(multisig_signer, multisig_account, &multisig, program_id)?;
+    validate_signer(multisig_signer, multisig_account, &multisig, multisig_account.owner)?;
 
     assert_that(transaction_account.is_writable, MultisigError::ImmutableTransactionAccount)?;
     assert_that(transaction.multisig == *multisig_account.key, MultisigError::InvalidTransactionAccount)?;
     assert_that(refundee.key != transaction_account.key, MultisigError::InvalidRefundeeAccount)?;
     assert_that(refundee.is_writable, MultisigError::ImmutableRefundeeAccount)?;
 
-    let approval_count = transaction.signers.iter()
-        .filter(|&did_sign| *did_sign)
-        .count() as u8;
+    let approval_count = transaction.signers.iter().filter(|&did_sign| *did_sign).count() as u8;
     assert_that(approval_count >= multisig.threshold, MultisigError::NotEnoughSigners)?;
 
-    let multisig_key = *multisig_account.key;
-    let multisig_signer_key = *multisig_signer.key;
-
-    // execute transaction
-    let signer_seeds = &[multisig_key.as_ref(), &[multisig.nonce]];
-    transaction
-        .instructions
-        .iter()
-        .map(|ix| {
-            let mut ix: Instruction = ix.into();
-            ix.accounts = ix
-                .accounts
-                .iter()
-                .map(|acc| {
-                    let mut acc = acc.clone();
-                    if acc.pubkey == multisig_signer_key {
-                        acc.is_signer = true;
-                    }
-                    acc
-                })
-                .collect();
-            solana_program::program::invoke_signed(&ix, accounts, &[&signer_seeds[..]])
-        })
-        .collect::<Result<Vec<_>, _>>()?;
-
-    close_account(transaction_account, refundee)
+    Ok(ValidatedAccounts { multisig_account, multisig_signer, transaction_account, refundee, multisig, transaction })
 }
